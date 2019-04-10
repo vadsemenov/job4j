1273195f9e1ebe54cf7e32854909bdb53f50d064
Vadim Semenov <5587394@mail.ru>
2019-04-10 14:44:01 +0300

    1.2. Подключить сборщик Maven[#4086]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Calculate{
+	public String echo (String value){
+	return String.format("%s %s %s", value, value, value);		
+	}
+	
+	public static void main (String[] args){
+		Calculate calc = new Calculate();
+		System.out.println(calc.echo("aah"));
+	}
+	
+}
