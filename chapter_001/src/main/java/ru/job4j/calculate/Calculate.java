093365f3f515998e28706fcd885ea246f68f0007
Vadim Semenov <5587394@mail.ru>
2019-04-11 19:42:31 +0300

    1.3. JavaDoc. Code convention[#131751]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,10 +1,27 @@
 package ru.job4j;
 
+
+/**
+ * Class Calculate решение задачи части 001 урок1
+ * @author Vadim Semenov
+ * @since 11.04.2019
+ */
 public class Calculate{
+
+
+	/**
+	 *Метод для тестирования
+	 * @param value строка для вывода в консоль
+	 * @return String value
+	 */
 	public String echo (String value){
 	return String.format("%s %s %s", value, value, value);		
 	}
-	
+
+	/**
+	 * Main
+	 * @param args -args
+	 */
 	public static void main (String[] args){
 		Calculate calc = new Calculate();
 		System.out.println(calc.echo("aah"));
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vadim Semenov (5587394@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
