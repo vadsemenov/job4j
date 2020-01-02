6d9631ab882022e8a68909aa77c257fc19483024
Vadim Semenov <5587394@mail.ru>
2020-01-02 22:12:18 +0300

    2. Реализовать класс Tracker[#131835]

---
+++ chapter_002/src/main/java/ru/job4j/Tracker/Tracker.java
@@ -0,0 +1,83 @@
+package ru.job4j.Tracker;
+
+import java.util.*;
+
+public class Tracker {
+    private final Item[] items = new Item[100];
+    private int count = 0;
+
+    /**
+     * Метод добавляет заявки в трекер.
+     *
+     * @param item - заявка
+     * @return Заявка
+     */
+    public Item add(Item item) {
+        item.setId(generateID());
+        return this.items[this.count++] = item;
+    }
+
+    /**
+     * Метод возвращает массив внесенных заявок.
+     *
+     * @return Массив внесенных заявок
+     */
+    public Item[] findAll() {
+        Item[] findedItem = new Item[this.items.length];
+        int size = 0;
+        for (int index = 0; index < this.items.length; index++) {
+            Item item = this.items[index];
+            if (item != null) {
+                findedItem[size] = item;
+                size++;
+            }
+        }
+        //System.arraycopy(this.items, 0, findedItem, 0, this.count);
+        return findedItem = Arrays.copyOf(findedItem, size);
+    }
+
+    /**
+     * Метод возвращает заявки по имени в трекере
+     *
+     * @param key
+     * @return
+     */
+    public Item[] findByName(String key) {
+        List<Item> findedItemList = new ArrayList<Item>();
+        for (int i = 0; i < this.count; i++) {
+            if (this.items[i].getName().equals(key)) {
+                findedItemList.add(this.items[i]);
+            }
+        }
+
+        Item[] findedByName = new Item[findedItemList.size()];
+        findedByName = findedItemList.toArray(findedItemList.toArray(findedByName));
+        return findedByName;
+    }
+
+    /**
+     * Метод возвращает заявку по Id.
+     *
+     * @param id -Id заявки
+     * @return Заявка
+     */
+    public Item findById(String id) {
+        for (int i = 0; i < this.count; i++) {
+            if (this.items[i].getId().equals(id)) {
+                return this.items[i];
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     *
+     * @return Уникальный ключ.
+     */
+    private String generateID() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/Tracker/TrackerTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.Tracker;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenFindAllThenReturnAllItems() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Item1");
+        Item item2 = new Item("Item2");
+        Item[] items = new Item[2];
+        items[0] = item1;
+        items[1] = item2;
+        tracker.add(item1);
+        tracker.add(item2);
+        Item[] result = tracker.findAll();
+        assertThat(result, is(items));
+    }
+
+    @Test
+    public void whenFindByNameReturnAllFindByName() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Item1");
+        Item[] items = new Item[1];
+        items[0] = item1;
+        tracker.add(item1);
+
+        Item[] result = tracker.findByName("Item1");
+        assertThat(result, is(items));
+    }
+
+    @Test
+    public void whenFindByIdReturnAllItemById() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Item1");
+        tracker.add(item1);
+        assertThat(item1, is(tracker.findById(item1.getId())));
+    }
+
+}
+
