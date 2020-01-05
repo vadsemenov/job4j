70ecf92d62466eb2ec30dbc2e48f0ff815f64a35
Vadim Semenov <5587394@mail.ru>
2020-01-05 17:04:53 +0300

    1. Используя класс ConsoleInput в классе StartUI обеспечить полноценную работу всего приложения[#131792]

--- chapter_002/src/main/java/ru/job4j/Tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/Tracker/StartUI.java
@@ -29,34 +29,36 @@ public class StartUI {
                 System.out.println("Input exist item Id:");
                 String oldId = scanner.nextLine();
                 Item item = new Item("newItem");
-                if(tracker.editItem(oldId,item)){
+                if (tracker.editItem(oldId, item)) {
                     System.out.println("Item succsfully edited.");
                 }
             } else if (select == 3) {
                 System.out.println("=== Delete Item ====");
                 System.out.println("Input item Id:");
                 String id = scanner.nextLine();
-                if(tracker.deleteItem(id)){
+                if (tracker.deleteItem(id)) {
                     System.out.println("Delete item with id " + id);
-                }else{
+                } else {
                     System.out.println("No one Item was deleted.");
                 }
             } else if (select == 4) {
                 System.out.println("=== Find item by Id ====");
                 System.out.println("Input item Id:");
                 String id = scanner.nextLine();
-                if (tracker.findById(id) != null) {
-                    System.out.println("Find item with id:" );
-                }else{
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println("Find item with id:" + item.getId());
+                } else {
                     System.out.println("No one Item was founded.");
                 }
             } else if (select == 5) {
                 System.out.println("=== Find items by name ====");
                 System.out.println("Input item name:");
                 String name = scanner.nextLine();
-                if (tracker.findByName(name) != null) {
-                    for (int i = 0; i < tracker.findByName(name).length ; i++) {
-                        System.out.println("Find item: "+ tracker.findByName(name)[i].getName());
+                Item[] items = tracker.findByName(name);
+                if (items != null) {
+                    for (int i = 0; i < items.length; i++) {
+                        System.out.println("Find item: " + items[i].getName());
                     }
                 }
             } else if (select == 6) {
