8c2a4205ec36603e00fe54a91296b57ffe8a6811
Vadim Semenov <5587394@mail.ru>
2020-01-12 16:44:20 +0300

    3. Интерфейс Input [#181092]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,26 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    private boolean drive = false;
+    private int passengers = 0;
+
+    @Override
+    public void drive() {
+        if (this.drive == false) {
+            this.drive = true;
+        } else {
+            this.drive=false;
+        }
+    }
+
+    @Override
+    public void passengers(int number) {
+     this.passengers+=number;
+    }
+
+    @Override
+    public double fuel(double liters) {
+        double priceLiter = 46.1d;
+        return liters*priceLiter;
+    }
+}
