d7149d1782577bd79dcd2d7c66e203c42e736b42
Vadim Semenov <5587394@mail.ru>
2020-01-21 21:31:50 +0300

    3. Замена if-else-throw на if-throw [#49895]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class ConsoleInput implements Input {
     Scanner scanner = new Scanner(System.in);
+
     @Override
     public String askStr(String question) {
         System.out.println(question);
@@ -18,10 +19,9 @@ public class ConsoleInput implements Input {
     @Override
     public int askInt(String question, int max) {
         int select = askInt(question);
-        if (select >= 0 && select < max) {
-            return select;
-        } else {
+        if (select < 0 && select > max) {
             throw new IllegalStateException(String.format("Out of about %s > [0, %s]", select, max));
         }
+        return select;
     }
 }
