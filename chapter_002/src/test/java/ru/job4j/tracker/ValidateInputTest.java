d2a882b786c5afacb355f7acfc7ca74b4fa07b05
Vadim Semenov <5587394@mail.ru>
2020-01-21 20:50:37 +0300

    1.1. Тесты на ValidateInput. [#223313]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        ByteArrayOutputStream mem = new ByteArrayOutputStream();
+        PrintStream out = System.out;
+        System.setOut(new PrintStream(mem));
+        String[] data = {"one", "1"};
+        ValidateInput input = new ValidateStubInput(data);
+        input.askInt("Enter");
+        assertThat(
+                mem.toString(),
+                is(String.format("Please enter validate data again.%n"))
+        );
+        System.setOut(out);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void whenInputMax(){
+        ByteArrayOutputStream mem = new ByteArrayOutputStream();
+        PrintStream out = System.out;
+        System.setOut(new PrintStream(mem));
+        String [] data ={"3"};
+
+        ValidateInput input = new ValidateStubInput(data);
+
+        input.askInt("Enter",2);
+        assertThat(mem.toString(),is(String.format("Please select key from menu.%n")));
+
+        System.setOut(out);
+    }
+}
