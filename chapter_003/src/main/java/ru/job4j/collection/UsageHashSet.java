73b1814d5d28fcb0efcd694628ccebe41296c626
Vadim Semenov <5587394@mail.ru>
2020-01-31 22:59:52 +0300

    0. Set, HashSet, Iterator. [#10095]

--- chapter_003/src/main/java/ru/job4j/collection/UsageHashSet.java
+++ chapter_003/src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -1,19 +1,17 @@
-//package ru.job4j.collection;
-//
-//import java.util.HashSet;
-//
-//public class UsageHashSet {
-//
-//    public static void main(String[] args) {
-//        HashSet<String> autos = new HashSet<String>();
-//        autos.add("Lada");
-//        autos.add("BMW");
-//        autos.add("Volvo");
-//        autos.add("Toyota");
-//        for (String auto : autos) {
-//            System.out.println(auto);
-//        }
-//        //2. Добавьте в коллекцию autos марки машин: Lada, BMW, Volvo, Toyota.
-//        //3. Добавьте цикл for-each и выведите элементы коллекции HashSet на консоль.
-//    }
-//}
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UsageHashSet {
+
+    public static void main(String[] args) {
+        HashSet<String> autos = new HashSet<String>();
+        autos.add("Lada");
+        autos.add("BMW");
+        autos.add("Volvo");
+        autos.add("Toyota");
+        for (String auto : autos) {
+            System.out.println(auto);
+        }
+    }
+}
