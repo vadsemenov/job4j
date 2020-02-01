e7c2a3bd1474b27209dfc78777b6986d8642222c
Vadim Semenov <5587394@mail.ru>
2020-02-01 17:07:44 +0300

    2. Преобразования List в Map. [#10093]

---
+++ chapter_003/src/main/java/ru/job4j/collection/OrderConvert.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class OrderConvert {
+    public static HashMap<String, Order> process(List<Order> orders) {
+        HashMap<String, Order> map = new HashMap<>();
+        // for orders
+        for (Order order : orders) {
+            map.put(order.getNumber(),order);
+        }
+
+//        Внутри метода process нужно пройти по всем элементам списка orders и вставить их в map.
+//        Чтобы пройти по элементам orders используйте цикл for-earch.
+//        Чтобы вставить данные в map используйте метод map.put. Первый параметр будет номер,
+//        а второй заявка.
+
+        return map;
+    }
+}
