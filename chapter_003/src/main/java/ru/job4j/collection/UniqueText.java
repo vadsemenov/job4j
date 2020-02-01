d0b0b60342a03aba3a5444d43a1112608c040b6e
Vadim Semenov <5587394@mail.ru>
2020-02-01 15:49:39 +0300

    2. Идентичные тексты. [#212655]

---
+++ chapter_003/src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        // for-each origin -> new HashSet.
+        for (String word : origin) {
+            check.add(word);
+        }
+        // for-each text -> hashSet.contains
+        for (String word : text) {
+            if (!check.contains(word)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
