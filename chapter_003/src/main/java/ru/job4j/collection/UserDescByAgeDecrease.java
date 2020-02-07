d9d23358ac36f35f5f1787591f36974e192900de
Vadim Semenov <5587394@mail.ru>
2020-02-07 22:57:17 +0300

    2. Комбинированный компаратор. [#10036]

---
+++ chapter_003/src/main/java/ru/job4j/collection/UserDescByAgeDecrease.java
@@ -0,0 +1,10 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class UserDescByAgeDecrease implements Comparator<User> {
+    @Override
+    public int compare(User o1, User o2) {
+        return Integer.compare(o2.getAge(), o1.getAge());
+    }
+}
--- chapter_003/src/test/java/ru/job4j/collection/UserTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/UserTest.java
@@ -2,11 +2,13 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -29,4 +31,58 @@ public class UserTest {
                 );
         assertThat(rsl, greaterThan(0));
     }
+
+    @Test
+    public void whenCompatorByNameIncrease() {
+        User user1 = new User("User1", 10);
+        User user2 = new User("User2", 20);
+        int rsl = new UserDescByNameIncrease().compare(user1, user2);
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenCompatorByNameDecrease() {
+        User user1 = new User("User1", 10);
+        User user2 = new User("User2", 20);
+        int rsl = new UserDescByNameDecrease().compare(user1, user2);
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenCompatorByAgeIncrease() {
+        User user1 = new User("User1", 10);
+        User user2 = new User("User2", 20);
+        int rsl = new UserDescByAgeIncrease().compare(user1, user2);
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenCompatorByAgeDecrease() {
+        User user1 = new User("User1", 10);
+        User user2 = new User("User2", 20);
+        int rsl = new UserDescByAgeDecrease().compare(user1, user2);
+        assertThat(rsl, greaterThan(0));
+    }
+
+
+    @Test
+    public void whenCompatorByNameAndAgeIncrease() {
+        Comparator<User> cmpNamePriority = new UserDescByNameIncrease().thenComparing(new UserDescByAgeIncrease());
+        int rsl = cmpNamePriority.compare(
+                new User("User1", 10),
+                new User("User2", 20)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenCompatorByNameAndAgeDecrease() {
+        Comparator<User> cmpNamePriority = new UserDescByNameDecrease().thenComparing(new UserDescByAgeDecrease());
+        int rsl = cmpNamePriority.compare(
+                new User("User1", 10),
+                new User("User2", 20)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
 }
