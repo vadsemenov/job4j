42783f423994276541c52935a72f7149a4edb776
Vadim Semenov <5587394@mail.ru>
2020-02-22 10:20:59 +0300

    5. Подсчет функции в диапазоне. [#24251]

--- chapter_004/src/test/java/ru/job4j/lambda/FunctionTypeTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/FunctionTypeTest.java
@@ -18,14 +18,14 @@ public class FunctionTypeTest {
     }
 
     @Test
-    public void whenQuadraticFunctionThenQuadraticResults() {
-        List<Double> result = FunctionType.diapason(5, 8, x ->Math.log(x));
+    public void whenLogarithmicFunctionThenLogarithmicResults() {
+        List<Double> result = FunctionType.diapason(5, 8, x -> Math.log(x));
         List<Double> expected = Arrays.asList(1.6094379124341003D, 1.791759469228055D, 1.9459101490553132D);
         assertThat(result, is(expected));
     }
 
     @Test
-    public void whenLogarithmicFunctionThenLogarithmicResults() {
+    public void whenQuadraticFunctionThenQuadraticResults() {
         List<Double> result = FunctionType.diapason(5, 8, x -> 2 * Math.pow(x, 2) + x + 1);
         List<Double> expected = Arrays.asList(56D, 79D, 106D);
         assertThat(result, is(expected));
