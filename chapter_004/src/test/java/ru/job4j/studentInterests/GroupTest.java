5bdf2b9325d3f5392f021cf73048569fdc522b0d
Vadim Semenov <5587394@mail.ru>
2020-04-10 14:12:02 +0300

    Группировка по интересам. [#106993]

---
+++ chapter_004/src/test/java/ru/job4j/studentInterests/GroupTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.studentInterests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.is;
+
+public class GroupTest {
+
+    @Test
+    public void whenGroupByInterest() {
+        Student andrey = new Student("Andrey", Set.of("Волейбол", "Баскетбол", "Футбол"));
+        Student mihail = new Student("Mihail", Set.of("Шахматы", "Волейбол", "Футбол"));
+        Student anatoliy = new Student("Anatoliy", Set.of("Шахматы", "Баскетбол"));
+
+        Map<String, Set<String>> groups = new HashMap<>();
+        groups.put("Баскетбол", Set.of("Andrey", "Anatoliy"));
+        groups.put("Шахматы", Set.of("Mihail", "Anatoliy"));
+        groups.put("Футбол", Set.of("Mihail", "Andrey"));
+        groups.put("Волейбол", Set.of("Mihail", "Andrey"));
+
+        Assert.assertThat(groups, is(Group.sections(List.of(andrey, anatoliy, mihail))));
+    }
+}
