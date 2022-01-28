bd2e787eb6747cefa9facab75622005d10f3ea65
Vadim <5587394@mail.ru>
2022-01-29 00:27:20 +0300

    3.3. Фиксация изменений на github [#243564]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -12,13 +12,14 @@ public class ArrayChar {
 
     /**
      * Проверяет, что слово начинается с префикса.
+     *
      * @param prefix префикс.
      * @return если слово начинается с префикса
      */
     public boolean startWith(String prefix) {
         boolean result = true;
         char[] value = prefix.toCharArray();
-        // проверить, что массив data имеет первые элементы, одинаковые с value
+
         for (int i = 0; i < value.length; i++) {
             if (data[i] != value[i]) {
                 result = false;
