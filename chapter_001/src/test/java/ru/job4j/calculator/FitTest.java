94d78b0cd41d738bab45f0070d05df490d9100a5
Vadim Semenov <5587394@mail.ru>
2022-02-03 12:03:12 +0300

    2. Тесты для задачи "идеальный вес" [#159458]

--- chapter_001/src/test/java/ru/job4j/calculator/FitTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/FitTest.java
@@ -1,9 +1,7 @@
 package ru.job4j.calculator;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.hamcrest.number.IsCloseTo.closeTo;
-import static org.junit.Assert.assertThat;
 
 public class FitTest {
 
@@ -11,13 +9,17 @@ public class FitTest {
     public void manWeight() {
         Fit fit = new Fit();
         double weight = fit.manWeight(180);
-        assertThat(weight, closeTo(92.0, 0.1));
+        double expected = 92.0;
+
+        Assert.assertEquals(expected, weight, 0.01);
     }
 
     @Test
     public void womanWeight() {
         Fit fit = new Fit();
         double weight = fit.womanWeight(170);
-        assertThat(weight, closeTo(69.0, 0.1));
+        double expected = 69.0;
+
+        Assert.assertEquals(expected, weight, 0.01);
     }
 }
