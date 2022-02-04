8e2da9063288038c986d53898018a3ad5fb8eb1c
Vadim Semenov <5587394@mail.ru>
2022-02-04 22:13:26 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,30 +1,48 @@
 package ru.job4j.array;
 
+
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
 
     @Test
     public void whenArrayHas5Then0() {
-        FindLoop find = new FindLoop();
         int[] input = new int[]{5, 10, 3};
         int value = 5;
-        int result = find.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value);
         int expect = 0;
-        assertThat(result, is(expect));
+        Assert.assertEquals(expect, result);
     }
 
     @Test
     public void whenArrayHas5ThenDontFind() {
-        FindLoop find = new FindLoop();
         int[] input = new int[]{6, 10, 3};
         int value = 5;
-        int result = find.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value);
         int expect = -1;
-        assertThat(result, is(expect));
+        Assert.assertEquals(expect, result);
+    }
+
+    @Test
+    public void whenArrayHasLength5Then0() {
+        int[] data = new int[]{5, 10, 3};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] data = new int[]{5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
     }
 }
 
