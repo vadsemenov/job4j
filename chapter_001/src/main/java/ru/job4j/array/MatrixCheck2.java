0a4661450df44b341941f7bce9258a25f9991ba1
Vadim Semenov <5587394@mail.ru>
2022-02-05 00:48:54 +0300

    6.7.3. Массив из диагонали матрицы. [#214128]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck2.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck2.java
@@ -22,4 +22,14 @@ public class MatrixCheck2 {
         }
         return result;
     }
+
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+
+        for (int i = 0; i < board.length; i++) {
+            rsl[i] = board[i][i];
+        }
+
+        return rsl;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheck2Test.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheck2Test.java
@@ -51,4 +51,40 @@ public class MatrixCheck2Test {
         boolean result = MatrixCheck2.monoVertical(input, column);
         Assert.assertFalse(result);
     }
+
+    @Test
+    public void whenDiagonalFullX() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'},
+        };
+        char[] result = MatrixCheck2.extractDiagonal(input);
+        char[] expected = {'X', 'X', 'X'};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenDiagonalFullOne() {
+        char[][] input = {
+                {'1', ' ', ' '},
+                {' ', '1', ' '},
+                {' ', ' ', '1'},
+        };
+        char[] result = MatrixCheck2.extractDiagonal(input);
+        char[] expected = {'1', '1', '1'};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenDiagonalMix() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'Y', ' '},
+                {' ', ' ', 'Z'},
+        };
+        char[] result = MatrixCheck2.extractDiagonal(input);
+        char[] expected = {'X', 'Y', 'Z'};
+        Assert.assertArrayEquals(expected, result);
+    }
 }
