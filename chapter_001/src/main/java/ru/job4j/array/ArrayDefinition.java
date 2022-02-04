25226d1356acb8bddb616e586ab2ea4e319a9bce
Vadim Semenov <5587394@mail.ru>
2022-02-04 16:53:48 +0300

    6.0.1.1. Размер массива [#333582]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+
+        short[] ages = new short[10];
+        String[] surnames = new String[10500];
+        float[] prices = new float[40];
+
+        System.out.println("Размер массива ages равен: " + ages.length);
+        System.out.println("Размер массива surnames равен: " + surnames.length);
+        System.out.println("Размер массива prices равен: " + prices.length);
+    }
+}
