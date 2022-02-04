cbdc489bef51fe253076019d2aeafbe7a79ba9c4
Vadim Semenov <5587394@mail.ru>
2022-02-04 14:34:24 +0300

    3. Тесты для определения расстояния между точками. [#159459 #260032]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -3,9 +3,6 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
 public class PointTest {
 
     @Test
@@ -24,13 +21,5 @@ public class PointTest {
         Point point = new Point(0, 0);
         double result = point.distance(point);
         Assert.assertEquals(result, 0D, 0.01);
-    }
-
-    @Test
-    public void whenShowInfo() {
-        Point first = new Point(1, 1);
-        first.info();
-        Point second = new Point(2, 2);
-        second.info();
     }
 }
