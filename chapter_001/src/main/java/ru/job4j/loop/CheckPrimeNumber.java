0c8f69bec8a242ca6207b458b2a76064e99c47c4
Vadim Semenov <5587394@mail.ru>
2022-02-05 20:49:34 +0300

    Оператор прерывания цикла - break

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,19 +3,12 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
 
-        if (number < 1) {
-            return false;
-        }
-
-        if (number == 1) {
-            return false;
-        } else {
-            for (int i = 2; i < number; i++) {
-                if (number % i == 0) {
-                    return false;
-                }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
             }
         }
-        return true;
+
+        return number > 1;
     }
 }
